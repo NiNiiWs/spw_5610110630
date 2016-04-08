8f1c50a8042a2a6c42f154335724bfc76d6d6735
NiNiiWs <alunnice2537@gmail.com>
2016-04-09 01:35:39 +0700

    fixed main add GamePanel
    เขียนกำหนด panel ใหม่ แยกไฟล์

--- Main.java
+++ Main.java
@@ -1,14 +1,13 @@
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
 import java.awt.Color;
+import javax.swing.JFrame;
 
-public class Main{
+public class Main {
 	public static void main(String[] args){
-
 		JFrame frame = new JFrame("Banana Wars");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(500, 800);
-		frame.getContentPane().setBackground(Color.BLACK);
+		frame.setSize(400, 650);
+		frame.getContentPane().setLayout(new BorderLayout());
 		frame.setVisible(true);
 	}
-
 }
